4b21ea20624296afa2e4f36c1f6ebc1feaa4d7f5
mtauha <muhammad.tauha@outlook.com>
2026-09-18 23:26:37 +0500

    add staging and marts BigQuery datasets

--- infra/bigquery.tf
+++ infra/bigquery.tf
@@ -5,3 +5,19 @@ resource "google_bigquery_dataset" "raw" {
 
   depends_on = [google_project_service.required["bigquery.googleapis.com"]]
 }
+
+resource "google_bigquery_dataset" "staging" {
+  project    = var.gcp_project
+  dataset_id = "staging"
+  location   = var.bq_location
+
+  depends_on = [google_project_service.required["bigquery.googleapis.com"]]
+}
+
+resource "google_bigquery_dataset" "marts" {
+  project    = var.gcp_project
+  dataset_id = "marts"
+  location   = var.bq_location
+
+  depends_on = [google_project_service.required["bigquery.googleapis.com"]]
+}
